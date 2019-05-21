a09f980d55248c0420aa96497ad60bc236f64cc5
akshirapov <cccp2006_06@mail.ru>
2019-05-21 19:05:56 +0800

    passed new material

---
+++ automate-the-boring-stuff-with-python/littleKid.py
@@ -0,0 +1,9 @@
+name = input('Name: ')
+age = int(input('Age: '))
+
+if name == 'Alice':
+    print('Hello Alice')
+elif age < 12:
+    print('You are not Alice, kiddo')
+else:
+    print('You are neither Alice nor a little kid.')
