adbcb0b0fba616940a7f9f842cf9d0d3a5e70a24
Alexander Shirapov <cccp2006_06@mail.ru>
2019-05-25 20:41:56 +0800

    Completed project "The Collatz sequence"

---
+++ Practice Projects/the-collatz-sequence/collatzSequence.py
@@ -0,0 +1,26 @@
+import sys
+
+def collatz(number):
+
+    result = 0
+    
+    if number%2 == 0:
+        result = number // 2
+    elif number%2 == 1:
+        result = 3 * number + 1
+
+    print(result)
+    return result
+
+try:
+    number = int(input('Enter number: \n'))
+except ValueError:
+    print('You must enter integer.')
+    sys.exit()
+    
+value = collatz(number)
+while True:
+    if value == 1:
+        break
+    else:
+        value = collatz(value)
