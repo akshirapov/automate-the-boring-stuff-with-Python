e454d6a5d5d4129ac87978b525e833d0f28b9fcb
Alexander Shirapov <cccp2006_06@mail.ru>
2019-05-27 16:27:01 +0800

    Studied new material

--- Examples/sandbox.py
+++ Examples/sandbox.py
@@ -1,10 +1,16 @@
-global eggs
+allGuests = {'Alice': {'apples': 5, 'pretzels': 12},
+             'Bob': {'ham sandwiches': 3, 'apples': 2},
+             'Carol': {'cups': 3, 'apple pies': 1}}
 
-eggs = 3
+def totalBrought(guests, item):
+    numBrought = 0
+    for k, v in guests.items():
+        numBrought = numBrought + v.get(item, 0)
+    return numBrought
 
-def spam():
-    eggs = 2
-    print(eggs)
-
-spam()
-print(eggs)
+print('Number of things being brought:')
+print(' - Apples: ' + str(totalBrought(allGuests, 'apples')))
+print(' - Cups: ' + str(totalBrought(allGuests, 'cups')))
+print(' - Cakes: ' + str(totalBrought(allGuests, 'cakes')))
+print(' - Ham Sandwiches: ' + str(totalBrought(allGuests, 'ham sandwiches')))
+print(' - Apple Pies: ' + str(totalBrought(allGuests, 'apple pies')))
