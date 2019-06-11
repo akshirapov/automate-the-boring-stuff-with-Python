7c3aedadb1cbf2f8808788996dee587c4ab3d22a
Alexander Shirapov <cccp2006_06@mail.ru>
2019-06-11 12:19:05 +0800

    Completed project "Filling In The Gaps"

--- Practice Projects/fill-gaps/fill_gaps.py
+++ Practice Projects/fill-gaps/fill_gaps.py
@@ -4,9 +4,10 @@
 
 import os
 import re
+import shutil
 
 
-def files_with_prefix(folder, prefix=''):
+def files_with_prefix(folder='.', prefix=''):
 
     files = []
 
@@ -26,11 +27,10 @@ def files_with_prefix(folder, prefix=''):
 
 def gaps_in_files(files):
 
-    if files is None:
+    if not files:
         return []
 
-    pattern = r'(\d)+'
-    num_regex = re.compile(pattern)
+    num_regex = re.compile(r'(\d)+')
 
     gaps_index = []
     for i in range(len(files)):
@@ -50,31 +50,62 @@ def gaps_in_files(files):
 
         diff = current_number - last_number
         if diff > 1:
-            gaps_index.append(i-1)
+            gaps_index.append(i)
 
     return gaps_index
 
 
-def rename_files(files, index=0):
+def rename_files(files, index=0, folder='.', prefix=''):
 
-    if files is None:
-        return []
+    if not files:
+        return
+
+    num_regex = re.compile(r'((\d)+)')
+    mo = num_regex.search(files[index-1])
+    if mo is None:
+        return
+
+    number = int(mo.groups()[1])
+
+    # temp folder for copies
+    temp_folder = os.path.join(folder, 'temp')
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
 
     for filename in files[index:]:
-        print(filename)
-        # TODO: Rename file
+
+        number += 1
+
+        new_filename = prefix + '{:03d}'.format(number) + '.txt'
+
+        print('RENAME:', filename, '--->', new_filename)
+
+        # move into temp folder
+        src_path = os.path.join(folder, filename)
+        dst_path = os.path.join(temp_folder, new_filename)
+        shutil.move(src_path, dst_path)
+
+    # move copies from temp folder
+    for copy_filename in os.listdir(temp_folder):
+
+        src_path = os.path.join(temp_folder, copy_filename)
+        shutil.move(src_path, folder)
+
+    if os.path.exists(temp_folder):
+        os.rmdir(temp_folder)
 
 
-target_folder = 'C:\\Users\\User\\Desktop\\github\\test'
+# target_folder = 'C:\\test'
+target_folder = '/home/alex/test'
 target_prefix = 'spam'
 
+print('FILES:')
 searched_files = files_with_prefix(target_folder, target_prefix)
 print(searched_files)
 
-searched_gaps = gaps_in_files(searched_files, target_prefix)
+print('INDEXES OF GABS:')
+searched_gaps = gaps_in_files(searched_files)
 print(searched_gaps)
 
-rename_files(searched_files, searched_gaps[0])
-
-#searched_files = files_with_prefix(target_folder, target_prefix)
-#print(searched_files)
+if searched_gaps:
+    rename_files(searched_files, searched_gaps[0], target_folder, target_prefix)
