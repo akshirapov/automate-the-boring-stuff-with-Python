943911d8afbdc95531282c15bef6b19155e952bf
Alexander Shirapov <cccp2006_06@mail.ru>
2019-06-21 13:22:51 +0800

    Studied new material

--- Examples/sandbox.py
+++ Examples/sandbox.py
@@ -1,6 +1,14 @@
-print('How are you?')
-feeling = input()
-if feeling.lower() == 'great':
-    print('I feel great too.')
-else:
-    print('I hope the rest of your day is good')
+import PyPDF2
+pdfFile = open('meetingminutes.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFile)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for pageNum in range(pdfReader.numPages):
+    pdfWriter.addPage(pdfReader.getPage(pageNum))
+
+pdfWriter.encrypt('swordfish')
+resultPdf = open('ecryptedminutes.pdf', 'wb')
+pdfWriter.write(resultPdf)
+
+pdfFile.close()
+resultPdf.close()
