ae44dc771309d809952c69780c658397ed07b45b
Alexander Shirapov <cccp2006_06@mail.ru>
2019-07-11 17:16:40 +0800

    Studied new material

--- Examples/sandbox.py
+++ Examples/sandbox.py
@@ -1,6 +1,8 @@
-import datetime
-import time
+import pyautogui
+from time import sleep
 
-halloween2016 = datetime.datetime(2019, 10, 31, 0, 0, 0)
-while datetime.datetime.now() < halloween2016:
-    time.sleep(1)
+sleep(5)
+for i in range(10):
+    sleep(0.5)
+    pyautogui.scroll(-2000)
+
